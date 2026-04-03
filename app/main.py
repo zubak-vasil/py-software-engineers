d38cb73f7bc32b933e68158c346e4d90f8200ead
zubak-vasil <zvasil7@gmail.com>
2026-04-03 23:37:20 +0300

    fixed1 solution

--- app/main.py
+++ app/main.py
@@ -38,8 +38,6 @@ class AndroidDeveloper(SoftwareEngineer):
 
 
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
-    def __init__(self, name: str) -> None:
-        super().__init__(name)
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
